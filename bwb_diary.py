d154e50ba629fd18fc3a96942553608243bdff81
SunyataZero <tord.dellsen@gmail.com>
2017-04-04 22:09:28 +0200

    Created a custom list with click detection!

--- bwb_diary.py
+++ bwb_diary.py
@@ -8,7 +8,6 @@ from PyQt5 import QtGui
 
 ADD_NEW_HEIGHT_IT = 50
 
-
 class DiaryListCompositeWidget(QtWidgets.QWidget):
     """
     Inspiration for this class:
@@ -25,20 +24,32 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
         super().__init__()
 
         self.v_box = QtWidgets.QVBoxLayout(self)
-        self.list_widget = QtWidgets.QListWidget()
-        ####layout_widget.setLayout(self.v_box)
         self.setLayout(self.v_box)
 
         hbox = QtWidgets.QHBoxLayout(self)
         self.v_box.addLayout(hbox)
         diary_label = QtWidgets.QLabel("<h3>Diary</h3>")
         hbox.addWidget(diary_label)
-        self.list_widget.setMinimumWidth(530)
         # -strange but we have to set a min width to avoid seeing the horizontal scrollbar
-        self.v_box.addWidget(self.list_widget)
-        self.list_widget.itemPressed.connect(self.on_item_pressed)  # Clicked doesn't work
-        self.list_widget.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
-        self.list_widget.setVerticalScrollMode(QtWidgets.QAbstractItemView.ScrollPerPixel)
+
+        self.my_widget = QtWidgets.QWidget()
+        self.scroll_area = QtWidgets.QScrollArea()
+        self.scroll_area.setWidget(self.my_widget)
+        self.scroll_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_list_vbox = QtWidgets.QVBoxLayout(self)
+        self.my_widget.setLayout(self.scroll_list_vbox)
+        self.scroll_list_vbox.addWidget(QtWidgets.QLabel("ASDF1"))
+        self.scroll_list_vbox.addWidget(QtWidgets.QLabel("ASDF2"))
+        self.scroll_list_vbox.addWidget(QtWidgets.QLabel("ASDF3"))
+        self.scroll_list_vbox.addWidget(QtWidgets.QLabel("ASDF4"))
+        self.scroll_list_vbox.addWidget(QtWidgets.QLabel("ASDF5"))
+        self.scroll_list_vbox.addStretch()
+        self.v_box.addWidget(self.scroll_area)
+
+        ###self.list_widget.itemPressed.connect(self.on_item_pressed)  # Clicked doesn't work
+        ###self.list_widget.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
+        ###self.list_widget.setVerticalScrollMode(QtWidgets.QAbstractItemView.ScrollPerPixel)
 
         # Adding new diary entry..
         # ..descriptive label
@@ -47,20 +58,6 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
         # ..question
         self.question_label = QtWidgets.QLabel()
         self.v_box.addWidget(self.question_label)
-        # ..labels
-        labels_hbox = QtWidgets.QHBoxLayout()
-        self.v_box.addLayout(labels_hbox)
-        self.gratitude_qpb = QtWidgets.QPushButton("Gratitude")
-        self.gratitude_qpb.setCheckable(True)
-        ##self.monday_qpb.setFixedWidth(BUTTON_WIDTH_IT) --- BUTTON_WIDTH_IT = 28
-        labels_hbox.addWidget(self.gratitude_qpb)
-        self.insight_qpb = QtWidgets.QPushButton("Insight")
-        self.insight_qpb.setCheckable(True)
-        labels_hbox.addWidget(self.insight_qpb)
-        self.dt_qpb = QtWidgets.QPushButton("Dharma talk")
-        self.dt_qpb.setCheckable(True)
-        labels_hbox.addWidget(self.dt_qpb)
-        labels_hbox.addStretch()
         # ..
         edit_diary_entry_hbox_l5 = QtWidgets.QHBoxLayout()
         self.v_box.addLayout(edit_diary_entry_hbox_l5)
@@ -79,15 +76,28 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
         self.adding_to_diary_now_button.pressed.connect(self.on_today_button_pressed)
         edit_diary_entry_vbox_l6.addWidget(self.adding_to_diary_now_button)
 
-
         self.adding_diary_entry_bn_w5 = QtWidgets.QPushButton("Add new diary entry")
         self.v_box.addWidget(self.adding_diary_entry_bn_w5)
         self.adding_diary_entry_bn_w5.clicked.connect(self.on_add_text_to_diary_button_clicked)
 
-    def on_item_pressed(self, i_listwidgetitem):
+
+
+    def on_custom_label_mouse_pressed(self, i_qmouseevent, i_diary_id_it):
+        print("button clicked: " + str(i_qmouseevent.button()))
+        print("diary id: " + str(i_diary_id_it))
+
+
+    def on_item_pressed(self, i_qmouseevent):
+        print("button clicked: " + str(i_qmouseevent.button()))
+
+        ##self.row_last_clicked
+        ###asdf
+
+        """
         row_index_it = i_listwidgetitem.listWidget().row(i_listwidgetitem)
         print("cell clicked -- row = " + str(row_index_it))
         self.row_last_clicked = i_listwidgetitem
+        """
 
     def contextMenuEvent(self, i_QContextMenuEvent):
         """
@@ -145,7 +155,14 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
             pass  # -do nothing
 
     def update_gui(self):
-        self.list_widget.clear()
+
+        # Clearing
+        ###self.list_widget.clear()
+        while self.scroll_list_vbox.count():
+            child_qlayoutitem = self.scroll_list_vbox.takeAt(0)
+            if child_qlayoutitem.widget():
+                child_qlayoutitem.widget().deleteLater()
+
         prev_diary_entry = None
 
         for diary_entry in bwb_model.DiaryM.get_all():
@@ -155,50 +172,25 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
             delimiter_sg = ", "
             if (prev_diary_entry is None) or (not is_same_day(prev_diary_entry.date_added_it, diary_entry.date_added_it)):
                 t_date_as_weekday_sg = datetime.datetime.fromtimestamp(diary_entry.date_added_it).strftime("%A")
-                list_item = QtWidgets.QListWidgetItem("     " + t_date_as_weekday_sg.title())
-                list_item.setFlags(list_item.flags() & ~ QtCore.Qt.ItemIsSelectable & QtCore.Qt.ItemIsUserCheckable)
-                self.list_widget.addItem(list_item)
+                listitem_cqll = CustomQLabel("     " + t_date_as_weekday_sg.title())
+                ##########listitem_cqll.setFlags(listitem_cqll.flags() & ~ QtCore.Qt.ItemIsSelectable & QtCore.Qt.ItemIsUserCheckable)
+                self.scroll_list_vbox.addWidget(listitem_cqll)
             karma_title_sg = ""
             label_text_sg = karma_title_sg\
                 + "[" + practice_for_diaryentry.title.strip(delimiter_sg) + "] "\
                 + diary_entry.diary_text.strip()
 
             # Setting up the display
-            list_item = QtWidgets.QListWidgetItem()
-            list_item.setData(QtCore.Qt.UserRole, diary_entry.id)  # to read: .data
-            row_layout_l7 = QtWidgets.QHBoxLayout()
-            row_label_w8 = QtWidgets.QLabel(label_text_sg)
-            ##row_label_w8.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
-            row_label_w8.setWordWrap(True)
-            row_layout_l7.addStretch()
-            row_layout_l7.addWidget(row_label_w8)
-            row_layout_l7.addStretch()
-            ###row_label_w8_label = QtWidgets.QLabel("Label")
-            ###row_layout_l7.addWidget(row_label_w8_label)
-            row_layout_l7.setContentsMargins(5, 5, 5, 5)
-            # -if this is not set we will get a default that is big and looks strange for a list
-            row_layout_l7.setSpacing(2)
-            row_w6 = QtWidgets.QWidget()
-            row_w6.setLayout(row_layout_l7)
-            row_w6.setSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.MinimumExpanding)
-            ###row_w6.setMaximumWidth(490)
-            ###row_w6.adjustSize()
-            list_item.setSizeHint(row_w6.sizeHint())
-            self.list_widget.addItem(list_item)
-            self.list_widget.setItemWidget(list_item, row_w6) # -http://doc.qt.io/qt-5/qlistwidget.html#setItemWidget
+            listitem_cqll = CustomQLabel(label_text_sg, diary_entry.id)
+            listitem_cqll.setWordWrap(True)
+            listitem_cqll.mouse_pressed_signal.connect(self.on_custom_label_mouse_pressed)
+            ###listitem_cqll.setProperty(DIARY_ENTRY_ID_PROPERTY, diary_entry.id)
 
-            """
-            if i_cur_sel_it == diary_entry.observance_ref:
-                #label.setFrameStyle(QFrame.StyledPanel)
-                # -http://doc.qt.io/qt-4.8/qframe.html#setFrameStyle
-                # -http://nullege.com/codes/search/PyQt4.QtGui.QFrame.setFrameStyle
-
-                #list_item.setFlags(list_item.flags() & ~ Qt.ItemIsSelectable)
-                ######################list_item.setBackground(QtCore.Qt.red) <-----------------
-            """
+            ############listitem_cqll.setData(QtCore.Qt.UserRole, diary_entry.id)  # to read: .data
+            self.scroll_list_vbox.addWidget(listitem_cqll)
 
             prev_diary_entry = diary_entry  # -used for the weekday labels
-        self.list_widget.scrollToBottom()
+        #####self.scroll_list_vbox.scrollToBottom() asdf
 
     def update_gui_date(self, i_unix_time_it = time.time()):
         """
@@ -222,3 +214,21 @@ def is_same_day(i_first_date_it, i_second_date_it):
     first_date = datetime.datetime.fromtimestamp(i_first_date_it)
     second_date = datetime.datetime.fromtimestamp(i_second_date_it)
     return first_date.date() == second_date.date()  # - == operator works for "datetime" type
+
+
+class CustomQLabel(QtWidgets.QLabel):
+    NO_DIARY_ENTRY_SELECTED = -1
+    diary_entry_id = NO_DIARY_ENTRY_SELECTED
+    mouse_pressed_signal = QtCore.pyqtSignal(QtGui.QMouseEvent, int)
+
+    def __init__(self, i_text_sg, i_diary_entry_id = NO_DIARY_ENTRY_SELECTED):
+        super().__init__(i_text_sg)
+        self.diary_entry_id = i_diary_entry_id
+
+    # Overridden
+    # Please note that this is not an event, but the event handler!
+    def mousePressEvent(self, i_qmouseevent):
+        self.mouse_pressed_signal.emit(i_qmouseevent, self.diary_entry_id)
+
+
+

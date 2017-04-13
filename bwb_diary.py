ae114e642460eb0826990a34f5e62422eae25b44
SunyataZero <tord.dellsen@gmail.com>
2017-04-13 12:02:52 +0200

    More code cleanup for bwb_diary.py

--- bwb_diary.py
+++ bwb_diary.py
@@ -7,11 +7,12 @@ from PyQt5 import QtCore
 from PyQt5 import QtGui
 
 
-MY_WIDGET_NAME = "test-name"
+MY_WIDGET_NAME_STR = "test-name"
 BACKGROUND_IMAGE_PATH_STR = "Gerald-G-Yoga-Poses-stylized-1-300px-CC0.png"
 NO_ENTRY_CLICKED_INT = -1
 
 
+# noinspection PyArgumentList
 class DiaryListCompositeWidget(QtWidgets.QWidget):
     """
     Inspiration for this class:
@@ -21,7 +22,7 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
     context_menu_change_date_signal = QtCore.pyqtSignal()
     context_menu_delete_signal = QtCore.pyqtSignal()
 
-    id_for_entry_last_clicked_it = NO_ENTRY_CLICKED_INT
+    last_entry_clicked_id_it = NO_ENTRY_CLICKED_INT
 
     def __init__(self):
         super().__init__()
@@ -31,11 +32,12 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
         self.scroll_area_w3.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.scroll_area_w3.setWidgetResizable(True)
         self.scroll_list_widget_w4 = QtWidgets.QWidget()
-        self.scroll_list_widget_w4.setObjectName(MY_WIDGET_NAME)
-        self.scroll_list_widget_w4.setStyleSheet("#" + MY_WIDGET_NAME
+        self.scroll_list_widget_w4.setObjectName(MY_WIDGET_NAME_STR)
+        self.scroll_list_widget_w4.setStyleSheet("#" + MY_WIDGET_NAME_STR
              + "{" + "background-image:url(\"" + BACKGROUND_IMAGE_PATH_STR
              + "\"); background-position:center; background-repeat:no-repeat" + "}")
         self.scroll_list_vbox_l5 = QtWidgets.QVBoxLayout()
+
         self.scroll_list_widget_w4.setLayout(self.scroll_list_vbox_l5)
         self.scroll_area_w3.setWidget(self.scroll_list_widget_w4)
         self.vbox_l2.addWidget(self.scroll_area_w3)
@@ -45,9 +47,10 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
     def on_custom_label_mouse_pressed(self, i_qmouseevent, i_diary_id_it):
         print("button clicked: " + str(i_qmouseevent.button()))
         print("diary id: " + str(i_diary_id_it))
-        self.id_for_entry_last_clicked_it = i_diary_id_it
+        self.last_entry_clicked_id_it = i_diary_id_it
 
-    def contextMenuEvent(self, i_QContextMenuEvent):
+    # noinspection PyUnresolvedReferences
+    def contextMenuEvent(self, i_qcontextmenuevent):
         """
         Overridden
         Docs: http://doc.qt.io/qt-5/qwidget.html#contextMenuEvent
@@ -69,7 +72,7 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
             self, "Remove diary entry?", "Are you sure that you want to remove this diary entry?"
         )
         if message_box_reply == QtWidgets.QMessageBox.Yes:
-            bwb_model.DiaryM.remove(int(self.id_for_entry_last_clicked_it))
+            bwb_model.DiaryM.remove(int(self.last_entry_clicked_id_it))
             self.update_gui()
             self.context_menu_delete_signal.emit()
         else:
@@ -79,7 +82,7 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
         """
         Docs: http://doc.qt.io/qt-5/qinputdialog.html#getText
         """
-        last_clicked_row_dbkey_it = int(self.id_for_entry_last_clicked_it)
+        last_clicked_row_dbkey_it = int(self.last_entry_clicked_id_it)
         diary_entry = bwb_model.DiaryM.get(last_clicked_row_dbkey_it)
         text_input_dialog = QtWidgets.QInputDialog()
         new_text_qstring = text_input_dialog.getText(
@@ -92,7 +95,7 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
             pass  # -do nothing
 
     def on_context_menu_change_date(self):
-        last_clicked_row_dbkey_it = int(self.id_for_entry_last_clicked_it)
+        last_clicked_row_dbkey_it = int(self.last_entry_clicked_id_it)
         diary_item = bwb_model.DiaryM.get(last_clicked_row_dbkey_it)
         updated_time_unix_time_it = bwb_date_time_dialog.DateTimeDialog.get_date_time_dialog(diary_item.date_added_it)
         if updated_time_unix_time_it != -1:
@@ -122,10 +125,8 @@ class DiaryListCompositeWidget(QtWidgets.QWidget):
                 listitem_cqll = CustomQLabel(label_text_sg, diary_entry.id)
                 listitem_cqll.setWordWrap(True)
                 listitem_cqll.mouse_pressed_signal.connect(self.on_custom_label_mouse_pressed)
-                ###listitem_cqll.setFixedWidth(400)
 
                 hbox_l6.addWidget(listitem_cqll, stretch=4)
-                #hbox_l6.addStretch()
 
         elif bwb_global.active_view_viewenum == bwb_global.ViewEnum.diary_daily_overview:
             for diary_entry in bwb_model.DiaryM.get_all_for_active_day():
@@ -172,7 +173,7 @@ class CustomQLabel(QtWidgets.QLabel):
     diary_entry_id = NO_DIARY_ENTRY_SELECTED
     mouse_pressed_signal = QtCore.pyqtSignal(QtGui.QMouseEvent, int)
 
-    def __init__(self, i_text_sg, i_diary_entry_id = NO_DIARY_ENTRY_SELECTED):
+    def __init__(self, i_text_sg, i_diary_entry_id=NO_DIARY_ENTRY_SELECTED):
         super().__init__(i_text_sg)
         self.diary_entry_id = i_diary_entry_id
 
